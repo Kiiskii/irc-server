d502346c8425e2f976067d605490c3f27eeeac70
Trang Pham <trpham@c2r2p5.hive.fi>
2025-11-18 16:48:22 +0200

    need to fix the forward declarration

--- inc/Channel.hpp
+++ inc/Channel.hpp
@@ -15,6 +15,25 @@
 #include "utils.hpp"
 #include "Server.hpp"
 
+
+enum	channelMsg
+{
+	NO_MSG,
+	JOIN_OK,
+	ALREADY_ON_CHAN,
+	CHANGE_TOPIC_MSG,
+	//below not use
+	INVITE_ONLY_CHAN,
+
+	// mode response, should move out??
+	SET_MODE_OK,
+	NO_ACTION,
+	UNKNOWN_MODE,
+	
+
+};
+
+
 class Client;
 
 /*
--- inc/utils.hpp
+++ inc/utils.hpp
@@ -5,23 +5,6 @@
 #include "Channel.hpp"
 #include "Server.hpp"
 
-enum	channelMsg
-{
-	NO_MSG,
-	JOIN_OK,
-	ALREADY_ON_CHAN,
-	CHANGE_TOPIC_MSG,
-	//below not use
-	INVITE_ONLY_CHAN,
-
-	// mode response, should move out??
-	SET_MODE_OK,
-	NO_ACTION,
-	UNKNOWN_MODE,
-	
-
-};
-
 
 std::ostream&				operator<<(std::ostream& os, 
 								const Channel& channel);
